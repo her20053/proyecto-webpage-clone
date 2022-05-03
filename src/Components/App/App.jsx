7f6585b80b53d6c67591acb14aef1d3b310bceff
Jose Andres Hernandez Guerra <her20053@uvg.edu.gt>
2022-05-03 02:54:12 -0600

    Creando ambiente para el proyecto

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './app.css'
+
+const App = () => {
+	return (
+		<div className="container">Hola que tal</div>
+	)
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const ruleForStyles = {
+	test: /\.css$/,
+	use: ['style-loader', 'css-loader']
+}
+module.exports = {
+	// entry: './src/index.js' <- Esta es la entrada por defecto
+	mode: "development",
+	devServer: {
+		static: "./build",
+	},
+	output: {
+		path: path.resolve(__dirname, 'build')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|mjs|jsx|ts|tsx)$/,
+				loader: 'babel-loader',
+				options: {
+					presets: [
+						[
+							'@babel/preset-react',
+							{
+								runtime: 'automatic'
+							}
+						]
+					]
+				}
+			}, ruleForStyles
+		]
+	}
+}
